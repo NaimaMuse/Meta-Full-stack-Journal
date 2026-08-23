a70ef5c6c17d1a207c8aaf9f1b4da6019b8fb7d0
NaimaMuse <Naciimaaahmed@gmail.com>
2026-08-23 19:04:04 +0300

    practiced with js functions

--- course-02-javascript/Module 2/functions.js
+++ course-02-javascript/Module 2/functions.js
@@ -12,9 +12,24 @@
 // }
 // add();
 
-function addtwoNum(a,b){
-    var c = a + b
-    console.log(c)
+// function addtwoNum(a,b){
+// //     var c = a + b
+// //     console.log(c)
+// // }
+// // addtwoNum(2,4)
+// // addtwoNum(4,6)
+
+//function with one parameter
+
+// function sayhello(name){
+//     return 'hello' + name
+// }
+// sayhello('naima')
+
+//function with one parameter
+function add(num1,num2){
+    return num1 + num2
+
 }
-addtwoNum(2,4)
-addtwoNum(4,6)
+var result = add(2,3)
+console.log(result)
