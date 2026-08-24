467a809e641564f64d8dce45a19dda35a611e1c3
NaimaMuse <Naciimaaahmed@gmail.com>
2026-08-24 14:29:58 +0300

    added new practical sheets

--- course-02-javascript/Module 2/objects.js
+++ course-02-javascript/Module 2/objects.js
@@ -1,50 +1,58 @@
 
-// let user = {
-//     name:'naima',
-//     age:25,
-//     email:'naciimaaahmed@gmail.com'
-// }
-// console.log(user)
-// console.log(user.name)
+// // let user = {
+// //     name:'naima',
+// //     age:25,
+// //     email:'naciimaaahmed@gmail.com'
+// // }
+// // console.log(user)
+// // console.log(user.name)
 
-// var house2 = {};
-// house2.rooms = 4;
-// house2.color = "pink";
-// house2.priceUSD = 12345;
-// console.log(house2.rooms)
+// // var house2 = {};
+// // house2.rooms = 4;
+// // house2.color = "pink";
+// // house2.priceUSD = 12345;
+// // console.log(house2.rooms)
 
 
-//Object literals and the brackets notation
+// //Object literals and the brackets notation
 
-// var user = {}
-// user['name'] = 'naima'
-// user['age'] = 25
-// console.log(user)
+// // var user = {}
+// // user['name'] = 'naima'
+// // user['age'] = 25
+// // console.log(user)
 
+
+// var car = {};
+// car.color = "red";
+// car["color"] = "green";
+// car["speed"] = 200;
+// car.speed = 100;
+// console.log(car); // {color: "green", speed: 100}
+
+
+
+
+
+
+
+// var clothes = []
+// clothes.push('pants')
+// clothes.push('Shirt')
+// clothes.push('Trousers')
+// clothes.push('Dress')
+// clothes.push('Jacket')
+// // clothes.pop(5)
+// console.log(clothes[2])
+
+// var favCar = {};
+// favCar.color = "red";
+// favCar.convertible = true;
 
 var car = {};
+
 car.color = "red";
-car["color"] = "green";
-car["speed"] = 200;
-car.speed = 100;
-console.log(car); // {color: "green", speed: 100}
 
-
-
-
-
-
-
-var clothes = []
-clothes.push('pants')
-clothes.push('Shirt')
-clothes.push('Trousers')
-clothes.push('Dress')
-clothes.push('Jacket')
-// clothes.pop(5)
-console.log(clothes[2])
-
-var favCar = {};
-favCar.color = "red";
-favCar.convertible = true;
-
+//add a method to the car object so that it can be called as car.turnkey()
+car.turnKey = function() { 
+  console.log('engine running'); 
+}
---
+++ course-02-javascript/Module 2/typeof.js
@@ -0,0 +1,21 @@
+// // var name = typeof('naima')
+// // console.log(name)
+
+// // var number = typeof(34)
+// // console.log(number)
+
+// // var boolean = typeof(true)
+// // console.log(boolean)
+
+// // var decimal = typeof(34.32)
+// // console.log(decimal)
+
+// // var test = [1,2,3,4]
+// // console.log(typeof(test))
+
+
+
+// var test = 1<2
+// console.log(typeof(test))
+var result = "Hello".indexOf('l');
+console.log(result)
