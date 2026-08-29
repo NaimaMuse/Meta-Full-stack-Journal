f1863cda72e66a0a91e8856b3f973e347c01beae
NaimaMuse <Naciimaaahmed@gmail.com>
2026-08-29 19:12:30 +0300

    message

--- course-02-javascript/Module 3/main.js
+++ course-02-javascript/Module 3/main.js
@@ -1,18 +1,66 @@
-// let x = 5;
-// let y = 6;
+// // // // // let x = 5;
+// // // // // let y = 6;
 
-// function exchange(a,b){
-//     return a * b
+// // // // // function exchange(a,b){
+// // // // //     return a * b
 
-// }
-// console.log(exchange(x,y));
+// // // // // }
+// // // // // console.log(exchange(x,y));
 
-// console.log(console.log('Hello'))
+// // // // // console.log(console.log('Hello'))
 
-function myDay() {
-    console.log('Morning');
-    console.log('Afternoon');
-    console.log('Evening');
-    myDay();
-}
-console.log(myDay)
+// // // // function myDay() {
+// // // //     console.log('Morning');
+// // // //     console.log('Afternoon');
+// // // //     console.log('Evening');
+// // // //     myDay();
+// // // // }
+// // // // console.log(myDay)
+// // // // var user = "Mary";
+// // // // var user = "Joanna";
+// // // // console.log(user)
+
+// // // let age = 20;
+
+// // // age = 21;
+
+// // // console.log(age);
+
+
+
+// // var globalVar = 77;
+
+// // function scopeTest() {
+// //         var localVar = 88;
+// //     }
+
+// // console.log(localVar);
+
+
+
+//     function meal(animal) {
+//         animal.food = animal.food + 10;
+//     }
+
+//     var dog = {
+//         food: 10
+//     };
+//     meal(dog);
+//     meal(dog);
+
+//     console.log(dog.food);
+
+
+    function two() {
+        return 2;
+    }
+
+    function one() {
+        return 1;
+    }
+
+    function calculate(initialValue, incrementValue) {
+        return initialValue() + incrementValue() + incrementValue();
+    }
+
+    console.log(calculate(two, one));
