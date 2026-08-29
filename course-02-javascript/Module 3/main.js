0936c15da89fca2e9cc56e14ba87da40686e9c12
NaimaMuse <Naciimaaahmed@gmail.com>
2026-08-29 18:16:11 +0300

    message

---
+++ course-02-javascript/Module 3/main.js
@@ -0,0 +1,18 @@
+// let x = 5;
+// let y = 6;
+
+// function exchange(a,b){
+//     return a * b
+
+// }
+// console.log(exchange(x,y));
+
+// console.log(console.log('Hello'))
+
+function myDay() {
+    console.log('Morning');
+    console.log('Afternoon');
+    console.log('Evening');
+    myDay();
+}
+console.log(myDay)
