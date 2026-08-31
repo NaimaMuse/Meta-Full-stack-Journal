6ec0d8ae549258f81353be36053d6505f8b7e63e
NaimaMuse <Naciimaaahmed@gmail.com>
2026-08-31 07:26:45 +0300

    created new files

---
+++ course-07-Programming in Python/Module 1/datatypes.py
@@ -0,0 +1,50 @@
+#1. Numeric Types 🔢
+
+# 1. int
+age = 20
+
+# 2. Float
+height = 1.66
+# 3. Complex
+z= 10+6j
+
+
+
+# 2. Sequence Types 📦
+
+# 1. String
+name = 'naima'
+
+# 2. list
+numbers = [1, 2, 3, 4]
+
+# 3. tuple
+numbers = (1, 2, 3, 4)
+# 👉 List = changeable
+# 👉 Tuple = unchangeable
+
+# 4. Dictionary
+dic ={
+    age:20,
+    name:'Naima'
+}
+print(dic[age])
+
+# 5. Boolean
+# True or False
+age = 20
+print(age > 18)
+
+# 6. Set
+numbers = {1, 2, 3, 3}
+# The duplicate 3 is removed.
+# Sets are:
+
+# Unordered
+# Not indexed
+# No duplicate values
+
+# 7. Function
+age = 20
+
+print(type(age))
